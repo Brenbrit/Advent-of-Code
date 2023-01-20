cf1b062d4b05a71d59297ba48ed82747759933e7
Brendan Guillory <brendanguillory@gmail.com>
2023-01-20 15:32:23 -0600

    Solved day 01

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,20 +1,52 @@
+use std::thread::current;
+
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut split = input.split("\n");
+    let split = input.lines();
+
+    let mut max = 0;
+    let mut current_total = 0;
 
     for s in split {
-        if s == "" {
-            println!("HI");
+        match s.parse::<u32>() {
+            Ok(num) => current_total += num,
+            Err(_) => {
+                if current_total > max {
+                    max = current_total;
+                }
+                current_total = 0
+            },
         }
     }
 
-    Some(0)
+    Some(max)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let split = input.lines();
+
+    let mut maxes = Vec::new();
+    let mut current_total = 0;
+
+    for s in split {
+        match s.parse::<u32>() {
+            Ok(num) => current_total += num,
+            Err(_) => {
+                maxes.push(current_total);
+                current_total = 0
+            },
+        }
+    }
+
+    if current_total != 0 {
+        maxes.push(current_total);
+    }
+
+    maxes.sort();
+    maxes.reverse();
+
+    Some(maxes[0] + maxes[1] + maxes[2])
 }
 
-use std::io::BufReader;
 fn main() {
     let input = &advent_of_code::read_file("inputs", 1);
     advent_of_code::solve!(1, part_one, input);
@@ -28,12 +60,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input).unwrap(), 24000 as u32);
+        assert_eq!(part_one(&input), Some(24000 as u32));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(45000 as u32));
     }
 }
