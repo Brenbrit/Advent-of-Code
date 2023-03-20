33f0fe0c55007336a169eadfc9a574c82e3bd9ad
Brendan Guillory <19159238+Brenbrit@users.noreply.github.com>
2023-03-20 10:25:14 -0500

    Solved Day 06 and scaffolded Day 07

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,35 +1,40 @@
 const SEQUENCE_START_LEN: usize = 4;
+const MESSAGE_START_LEN: usize = 14;
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut chars_read: u32;
-    let mut recent_letters = [' '; SEQUENCE_START_LEN];
+pub fn part_one(input: &str) -> Option<usize> {
+    find_start(input, SEQUENCE_START_LEN)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    find_start(input, MESSAGE_START_LEN)
+}
+
+fn find_start(input: &str, start_sequence_size: usize) -> Option<usize> {
+    let mut chars_read: usize;
+    let mut recent_letters = vec![' '; start_sequence_size];
     let input_chars: Vec<char> = input.chars().collect();
 
     // init array
-    for input_index in 0..SEQUENCE_START_LEN - 1 {
+    for input_index in 0..start_sequence_size - 1 {
         recent_letters[input_index] = *input_chars.get(input_index).unwrap();
     }
     // init chars_read
-    chars_read = (SEQUENCE_START_LEN - 1) as u32;
+    chars_read = (start_sequence_size - 1) as usize;
 
     // analyze sequence
-    for input_index in SEQUENCE_START_LEN - 1..input.len() {
+    for input_index in start_sequence_size - 1..input.len() {
         // Read next letter
         let next_letter = *input_chars.get(input_index).unwrap();
         chars_read += 1;
 
         // Write to recent_letters
-        recent_letters[input_index % SEQUENCE_START_LEN] = next_letter;
+        recent_letters[input_index % start_sequence_size] = next_letter;
 
         if ! contains_duplicates(&recent_letters)? {
             return Some(chars_read);
         }
     }    
 
-    None
-}
-
-pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
@@ -59,12 +64,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 6);
-        assert_eq!(part_one(&input), Some(10 as u32));
+        assert_eq!(part_one(&input), Some(10 as usize));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 6);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(29 as usize));
     }
 }
