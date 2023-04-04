3143eb2ff05ad388b2c00b61884ad498dbb27bfb
Brendan Guillory <19159238+Brenbrit@users.noreply.github.com>
2023-04-03 21:30:46 -0500

    Started solving Day 12 Part 2

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,8 +1,8 @@
-use std::collections::{HashMap, HashSet};
+use std::{collections::{HashMap, HashSet}, time::Instant};
 
 const MAX_STEP_UP: i32 = 1;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Graph {
     data: HashMap<String, u32>,
     visited_nodes: HashMap<(usize, usize), Vec<(usize, usize)>>,
@@ -51,8 +51,51 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(path_size)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let (topology, _, ending) = read_input(input)?;
+
+    let rows = topology.len();
+    let columns = topology
+        .get(0)?
+        .len();
+
+    let mut graph = generate_graph(topology.clone())?;
+
+    let mut min_elevation_points: Vec<(usize, usize)> = vec![];
+    let mut shortest_distance_from_min = u32::MAX;
+
+    for row in 0..rows {
+        for col in 0..columns {
+            if topology.get(row).unwrap()
+                .get(col).unwrap()
+                .clone()
+                == 0 {
+                min_elevation_points.push((row, col));
+            }
+        }
+    }
+
+    // Time this function
+    // it takes a long time.
+    let start_time = Instant::now();
+
+    for i in 0..min_elevation_points.len() {
+        let min_elevation_point = min_elevation_points.get(i).unwrap();
+        let (new_graph, path_size) = dijkstra_shortest_path(graph, min_elevation_point.clone(), ending)?;
+        if path_size < shortest_distance_from_min {
+            shortest_distance_from_min = path_size;
+        }
+        graph = new_graph;
+
+        println!("{}/{} ({:.2}%). Expected remaining time: {:.2} seconds.", 
+            i+1, 
+            min_elevation_points.len(), 
+            f64::from(((i+1)*100) as u32) / f64::from(min_elevation_points.len() as u32),
+            (start_time.elapsed().as_secs_f64() / f64::from((i as u32)+1)) * f64::from((min_elevation_points.len()-i) as u32),
+        );
+    }
+
+    Some(shortest_distance_from_min)
 }
 
 fn dijkstra_shortest_path(mut graph: Graph, starting: (usize, usize), ending: (usize, usize)) -> Option<(Graph, u32)> {
@@ -111,6 +154,35 @@ fn dijkstra_shortest_path(mut graph: Graph, starting: (usize, usize), ending: (u
 
     let shortest_path_cost = graph.get(starting, ending)?;
     Some((graph, shortest_path_cost))
+}
+
+fn reverse_dijkstra(graph: Graph, destination: (usize, usize)) -> Option<Graph> {
+    let mut tentative_distances: HashMap<(usize, usize), u32> = HashMap::new();
+    let mut unvisited_nodes: HashSet<(usize, usize)> = HashSet::new();
+
+    // mark all nodes to unvisited and set initial values to infinity
+    for node in graph.visited_nodes.keys() {
+        let node = node.clone();
+        unvisited_nodes.insert(node);
+        tentative_distances.insert(node, u32::MAX);
+    }
+
+    // Set distance from source to source = 0
+    tentative_distances.insert(destination, 0);
+
+    while ! unvisited_nodes.is_empty() {
+        // Find unvisited node closest to starting
+        let mut closest_node: (usize, usize) = (0, 0);
+        let mut closest_node_distance = u32::MAX;
+        for unvisited_node in &unvisited_nodes {
+            if *tentative_distances.get(unvisited_node).unwrap() < closest_node_distance {
+                closest_node = unvisited_node.clone();
+                closest_node_distance = *tentative_distances.get(unvisited_node).unwrap();
+            }
+        }
+    }
+
+    Some((graph))
 }
 
 fn generate_graph(topology: Vec<Vec<i32>>) -> Option<Graph> {
@@ -232,6 +304,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 12);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(29 as u32));
     }
 }
